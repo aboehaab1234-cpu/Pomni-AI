5039058808f8249b48aed60f0f22100aa9320b68
aboehaab1234-cpu <aboehaab1234@gmail.com>
2026-09-26 02:07:28 +0300

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ import sub from './sub.js';
 
 /* =========== Client ========== */
 const client = new Client({
-  phoneNumber: '201117873587', // Bot number
+  phoneNumber: '201117873587', // 𝑫𝑨𝑹𝑲 𝐁𝐎𝐓 🔱
   prefix: [".", "/", "!"],
   fromMe: false, 
   owners: [
@@ -14,9 +14,9 @@ const client = new Client({
   // Owner 2
     { name: "emam", lid: "221307316789354@lid", jid: "201117873587@s.whatsapp.net" },
   // Owner 3
-    { name: "Sukuna", jid: "201033024135@s.whatsapp.net", lid: "50414477168824@lid" },
+    { name: "Sukuna", jid: "201117873587@s.whatsapp.net", lid: "50414477168824@lid" },
   // Owner 4 
-   { name: "عمورتي", jid: "201050079089@s.whatsapp.net", lid: "51664513925368@lid" }
+   { name: "عمورتي", jid: "201117873587@s.whatsapp.net", lid: "51664513925368@lid" }
   ],
   settings: { noWelcome: false },
   commandsPath: './plugins'
@@ -34,7 +34,7 @@ if (!global.db) {
 const { config } = client;
 config.info = { 
   nameBot: "♡ 𝙋𝙊𝙈𝙉𝙄 🎪 〈", 
-  nameChannel: "𝐕𝐈𝐈7 ~ 𝐂𝐡𝐚𝐧𝐧𝐞𝐥 🕷️", 
+  nameChannel: "𝑫𝑨𝑹𝑲 𝑾𝑬𝑩 ~ 𝐂𝐡𝐚𝐧𝐧𝐞𝐥 🕷️", 
   idChannel: "120363225356834044@newsletter",
   urls: {
     repo: "https://github.com/deveni0/Pomni-AI",
